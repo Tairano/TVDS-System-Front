fba03e8bd35031b8c7117dc5efc8b6fed66aba0d
Tairano <2023684800@qq.com>
2023-06-14 00:26:03 +0800

    feat: 用户 车厢审核 审核功能调整 - 默认显示异常 - 可以选择正常部件 - 操作改完为 子弹窗处理 单独 设置状态 设置备注(未完成) - fix 表格 部件状态 修改为 机器初审结果 - fix 异常高亮 红绿

--- src/views/components/carriageAuditDialog.vue
+++ src/views/components/carriageAuditDialog.vue
@@ -7,29 +7,57 @@
       direction="vertical"
       :column="1">
     <el-descriptions-item>
-      <el-table :data="data.affiliateParts" style="width: 98%">
+      <el-collapse>
+        <el-collapse-item title="正常部件" >
+          <el-table :data="normalForm" style="width: 98%">
+            <el-table-column prop="id" label="部件id" align="center" width="300"/>
+            <el-table-column prop="partName" label="部件类型" align="center">
+              <template v-slot="scope">
+                {{toChinese(scope.row.partName)}}
+              </template>
+            </el-table-column>
+            <el-table-column prop="partName" label="机器初审结果" align="center">
+              <template v-slot="scope">
+                <el-button text="text" type="success" plain v-show="scope.row.status === 2">正常</el-button>
+                <el-button text="text" type="danger" plain v-show="scope.row.status === 1">异常</el-button>
+              </template>
+            </el-table-column>
+            <el-table-column prop="partName" label="审核情况" align="center">
+              <template v-slot="scope">
+                <el-button text="text" type="success" plain v-show="scope.row.status === 2">正常</el-button>
+                <el-button text="text" type="danger" plain v-show="scope.row.status === 1">异常</el-button>
+              </template>
+            </el-table-column>
+            <el-table-column label="操作" align="center">
+              <template v-slot="scope">
+                <el-button @click="viewImage(scope.row.imageUrl,scope.row)">审核</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-collapse-item>
+      </el-collapse>
+      <el-table :data="abnormalForm" style="width: 98%">
         <el-table-column prop="id" label="部件id" align="center" width="300"/>
         <el-table-column prop="partName" label="部件类型" align="center">
           <template v-slot="scope">
             {{toChinese(scope.row.partName)}}
           </template>
         </el-table-column>
-        <el-table-column prop="partName" label="部件状态" align="center">
+        <el-table-column prop="partName" label="机器初审结果" align="center">
           <template v-slot="scope">
-            {{statusToChinese(scope.row.status)}}
+            <el-button text="text" type="success" plain v-show="scope.row.status === 2">正常</el-button>
+            <el-button text="text" type="danger" plain v-show="scope.row.status === 1">异常</el-button>
           </template>
         </el-table-column>
-        <el-table-column label="部件详略图" align="center">
+        <el-table-column prop="partName" label="审核情况" align="center">
           <template v-slot="scope">
-            <el-button @click="viewImage(scope.row.imageUrl,scope.row)">查看大图</el-button>
+            <el-button text="text" type="success" plain v-show="scope.row.status === 2">正常</el-button>
+            <el-button text="text" type="danger" plain v-show="scope.row.status === 1">异常</el-button>
           </template>
         </el-table-column>
         <el-table-column label="操作" align="center">
           <template v-slot="scope">
-            <el-radio-group v-model="form.results[getIndexByDbId(scope.row.dbId)].status">
-              <el-radio label="正常" :v-model="changeToString(scope.row.status)"></el-radio>
-              <el-radio label="异常" :v-model="changeToString(scope.row.status)"></el-radio>
-            </el-radio-group>
+            <el-button @click="viewImage(scope.row.imageUrl,scope.row)">审核</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -42,25 +70,26 @@
     <template #extra>
       <div style="display: flex">
         <el-button @click="onSubmit" type="primary">提交审核</el-button>
-        <el-button @click="this.shows()">取消</el-button>
+        <el-button @click="this.shows(0)">取消</el-button>
       </div>
     </template>
   </el-descriptions>
   <el-dialog v-model="dialog">
-    <ImageInformation :ImageUrl="dialogImageUrl"
-                      :ImageInfo="dialogImageInfo">
-    </ImageInformation>
+    <image-audit :ImageUrl="dialogImageUrl"
+                 :ImageInfo="dialogImageInfo"
+                 :Func="sd">
+    </image-audit>
   </el-dialog>
 </template>
 
 <script>
 import {statusToChinese, toChinese} from "@/tool/utils";
 import {dwnImg, getCarriageAudit, getImg, getTmp, submitAuditResult} from "@/tool/api/methods";
-import ImageInformation from "@/views/components/compImageDialog.vue";
 import {ElMessage} from "element-plus";
+import ImageAudit from "@/views/components/compAuditDialog.vue";
 export default{
   name: "carriageAudit",
-  components: {ImageInformation},
+  components: {ImageAudit},
   data(){
     return{
       downLoadName: '',
@@ -77,6 +106,8 @@ export default{
         comment: '',
         results: []
       },
+      normalForm: [],
+      abnormalForm: [],
       data: {
         affiliateParts: []
       }
@@ -113,7 +144,7 @@ export default{
       }
       submitAuditResult(this.MissionId, this.form).then(
           response => {
-            this.shows()
+            this.shows(1)
             ElMessage({
               type: 'success',
               message: '已提交结果。'
@@ -153,7 +184,11 @@ export default{
             this.data = response
             this.form.results = []
             for(let i in this.data.affiliateParts){
-              this.form.results.push({partId: this.data.affiliateParts[i].dbId, status: this.changeToString(this.data.affiliateParts[i].status)})
+              if(this.data.affiliateParts[i].status === 2){
+                this.normalForm.push(this.data.affiliateParts[i])
+              }
+              else this.abnormalForm.push(this.data.affiliateParts[i])
+              this.form.results.push({partId: this.data.affiliateParts[i].dbId, status: this.changeToString(this.data.affiliateParts[i].status), comment: null})
             }
           }
       )
--- src/views/components/compAuditDialog.vue
+++ src/views/components/compAuditDialog.vue
@@ -2,6 +2,86 @@
   <el-card style="max-height:100vh; text-align: center; background-color: black; margin-bottom: 10px">
     <el-image  key="image" :src="getImg(ImageUrl)" lazy style="height: 100%; width: auto"/>
   </el-card>
+  <el-descriptions
+      title="详细信息"
+      direction="vertical"
+      :column="3"
+      border
+  >
+    <template #extra>
+      <div style="display: flex">
+        <div style="width: 10vw; text-align: center; margin-top: 7px">
+          保存为：
+        </div>
+        <el-input v-model="downLoadName"/>
+        <el-button @click="downloadImg" type="primary">下载图片到本地</el-button>
+      </div>
+    </template>
+    <el-descriptions-item label="拍摄日期">{{ImageInfo.createTime}}</el-descriptions-item>
+    <el-descriptions-item label="上传时间">{{ImageInfo.updateTime}}</el-descriptions-item>
+    <el-descriptions-item label="检测日期">{{ImageInfo.checkTime}}</el-descriptions-item>
+  </el-descriptions>
+  <el-descriptions
+      direction="vertical"
+      :column="4"
+      border
+  >
+    <el-descriptions-item label="所在车厢ID">{{ImageInfo.updateTime}}</el-descriptions-item>
+    <el-descriptions-item label="图片编号">{{ImageInfo.id}}</el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <div class="cell-item">
+          图片分类
+        </div>
+      </template>
+      <el-tag size="small">{{ImageInfo.model}}</el-tag>
+      <el-tag size="small">{{ImageInfo.partName}}</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="部件类型">{{toChinese(ImageInfo.partName)}}</el-descriptions-item>
+  </el-descriptions>
+  <el-descriptions
+      title="审核"
+      direction="vertical"
+      :column="3"
+      border>
+    <el-descriptions-item>
+      <template #label>
+        <div class="cell-item">
+          机器初审结果
+        </div>
+      </template>
+      <el-button text="text" type="success" plain v-show="ImageInfo.status === 2">正常</el-button>
+      <el-button text="text" type="danger" plain v-show="ImageInfo.status === 1">异常</el-button>
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <div class="cell-item">
+          状态
+        </div>
+      </template>
+      <el-radio-group v-model="radio">
+        <el-radio :label="2">正常</el-radio>
+        <el-radio :label="1">异常</el-radio>
+      </el-radio-group>
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <div class="cell-item">
+          备注
+        </div>
+      </template>
+      <el-input
+          type="textarea"
+          :rows="2"
+          placeholder="请输入内容"
+          v-model="comment">
+      </el-input>
+    </el-descriptions-item>
+  </el-descriptions>
+  <div style="text-align: right; width: 100%">
+      <el-button @click="onSubmit" type="primary">提交审核</el-button>
+      <el-button @click="this.shows(0)">取消</el-button>
+  </div>
 </template>
 
 <script>
@@ -14,11 +94,14 @@ export default{
     return{
       downLoadName: '',
       imageData: null,
+      radio: 2,
+      comment: null
     }
   },
   props: {
     ImageUrl: String,
-    ImageInfo: Object
+    ImageInfo: Object,
+    Func: function (){}
   },
   methods:{
     toChinese,
